03fa1626a428b1a3e1f02a1304b9d3ce8e5f9a72
Varun <varunvamin@gmail.com>
2026-07-02 12:46:24 +0530

    Docs: Add docstring to delete_meal function

--- nutrients.py
+++ nutrients.py
@@ -60,6 +60,7 @@ class NutritionalAnalyzer:
         self._save_data()
 
     def delete_meal(self, date_str: str, index: int) -> bool:
+        """Removes a specific meal from a given date's log by its index."""
         if date_str in self.data and 0 <= index < len(self.data[date_str]):
             self.data[date_str].pop(index)
             self._save_data()
